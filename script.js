621bf25f1280cf75bb0a37b8bd5b45ab98d6dfea
Alan Mabry <alan.mabry@gmail.com>
2019-04-06 13:02:56 -0400

    reset

--- script.js
+++ script.js
@@ -15,25 +15,37 @@ class Game {
     console.log('countDown', this.timerCount);
 
     if (this.timerCount >= this.timeLimit) {
-      this.stop();
+      this.reset();
     }
 
     return this.timerCount;
+  }
+
+  defaultButtons () {
+    document.querySelector('#play').style.display = 'inline-block';
+    document.querySelector('#stop').style.display = 'none';
+    document.querySelector('#reset').style.display = 'none';
   }
 
   gameTime() {
     switch(this.gameState) {
       case 'start':
         this.timer = setInterval(this.countDown.bind(this), 1000);
+        document.querySelector('#play').style.display = 'none';
+        document.querySelector('#stop').style.display = 'inline-block';
+        document.querySelector('#reset').style.display = 'inline-block';
         console.log('start');
       break;
       case 'stop':
         clearInterval(this.timer);
+        document.querySelector('#play').style.display = 'inline-block';
+        document.querySelector('#reset').style.display = 'block';
         console.log('stop');
       break;
       case 'reset':
         clearInterval(this.timer);
         this.timerCount = 0;
+        this.defaultButtons();
         console.log('reset');
       break;
       default:
@@ -49,6 +61,11 @@ class Game {
 
   stop() {
     this.gameState = 'stop';
+    this.gameTime();
+  }
+
+  reset() {
+    this.gameState = 'reset';
     this.gameTime();
   }
 
